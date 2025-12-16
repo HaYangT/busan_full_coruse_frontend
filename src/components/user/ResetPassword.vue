391881d593c01cbdf5193c715303967ff0d4c835
suwon choi <suwon1863@gmail.com>
2025-12-16 18:32:20 +0900

    비밀번호 초기화 요청

--- src/components/user/ResetPassword.vue
+++ src/components/user/ResetPassword.vue
@@ -32,11 +32,28 @@ import { ref } from "vue";
 const id = ref("");
 const email = ref("");
 
-/*
 const onReset = async () => {
-    엄청난 로직을 작성
+  
+  let  resultMessage = '';
+
+  try{
+    const baseUrl = import.meta.env.VITE_SERVER_URL;
+    const url = `${baseUrl}/api/v1/password/reset-request`
+    const response = await axios.post(url,{
+      email: email.value
+    })
+
+    resultMessage = response.data.message;
+    alert(resultMessage);
+  }
+  catch(error){
+    console.log(error)
+    resultMessage = error.response.data.message;
+    alert(resultMessage);
+  }
+
+  
 };
-*/
 </script>
 
 <style scoped>
